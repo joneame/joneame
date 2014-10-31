1227ded9867aef8a332530719e1e4195a0c6100e
wodim <neikokz@gmail.com>
2014-10-31 12:38:05 +0100

    Mejoro un poco el correo de contraseña olvidada

--- libs/mail.php
+++ libs/mail.php
@@ -31,18 +31,18 @@ function send_recover_mail ($user) {
     $now = time();
     $key = md5($user->id.$user->pass.$now.$site_key.get_server_name());
     $url = 'http://'.get_server_name().$globals['base_url'].'profile.php?login='.$user->username.'&t='.$now.'&k='.$key;
-    //echo "$user->username, $user->email, $url<br />";
+
     $to      = $user->email;
     $subject = _('Recuperación o verificación de la contraseña de '). get_server_name();
-    $message = $to .": para  poder acceder sin la clave, conéctate a la siguiente dirección en menos de dos horas:".$url."\n";
+    $message = $to .": para poder acceder sin la clave, conéctate a la siguiente dirección en menos de dos horas:\n".$url."\n\n";
     $message .= _('Pasado este tiempo puedes volver a solicitar acceso en: ') . "\nhttp://".get_server_name().$globals['base_url']."login.php?op=recover\n\n";
-    $message .= _('Una vez en tu perfil, puedes cambiar la clave de acceso.') . "\n" . "\n";
+    $message .= _('Una vez en tu perfil, puedes cambiar la clave de acceso.') . "\n";
     $message .= "\n\n". _('Este mensaje ha sido enviado a solicitud de la dirección: ') . $globals['user_ip'] . "\n\n";
-    $message .= "-- \n  " . _('la administración de joneame.net');
+    $message .= "-- " . _('la administración de joneame.net');
     $message = wordwrap($message, 70);
     $headers = 'Content-Type: text/plain; charset="utf-8"'."\n" . 'X-Mailer: joneame/PHP/' . phpversion(). "\n". 'From: admin@joneame.net <web@'.get_server_name().">\n";
-    //$pars = '-fweb@'.get_server_name();
-    mail($to, $subject, $message, $headers);
+
+    mb_send_mail($to, $subject, $message, $headers);
     echo '<p><strong>' ._ ('Correo enviado, mira tu buzón, allí están las instrucciones. Mira también en la carpeta de spam.') . '</strong></p>';
     return true;
 }
