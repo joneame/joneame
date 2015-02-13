3626e5543b110dbe291a66f815ffb0eb0be284b3
wodim <neikokz@gmail.com>
2015-02-13 15:42:34 +0100

    arreglo la posición del pie en el menú de admin

--- admin/index.php
+++ admin/index.php
@@ -13,12 +13,10 @@ include(mnminclude.'link.php');
 if ($current_user->admin) {
     do_header(_('Administración de Jonéame - Cómo mola ser capo de la mafia'));
     echo "<br/><br/>";
-    echo '<div class="genericform" style="text-align: center;"><h4>Il capos administration: todo lo que necesitáis para imponer la justicia</h4><fieldset class="fondo-caja"><a href="cortos.php"><img src="../img/panel/cortos.png" border=0 /></a> <a href="bans.php"><img src="../img/panel/ban.png" border=0 /></a> <br/><a href="http://devel.joneame.net"><img src="../img/panel/devel.png" border=0 /></a> <a href="historial.php"><img src="../img/panel/historial.png" border=0 /></a></div>';
+    echo '<div class="genericform" style="text-align: center;"><h4>Il capos administration: todo lo que necesitáis para imponer la justicia</h4><fieldset class="fondo-caja"><a href="cortos.php"><img src="../img/panel/cortos.png" border=0 /></a> <a href="bans.php"><img src="../img/panel/ban.png" border=0 /></a> <br/><a href="http://devel.joneame.net"><img src="../img/panel/devel.png" border=0 /></a> <a href="historial.php"><img src="../img/panel/historial.png" border=0 /></a></fieldset></div>';
 
     echo "<br/><br/><br/>";
 } else {
-     do_error(_('Esta página es sólo para administradores, sal de aquí, cojones ya. <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>Le llamaremos a Chuck Norris para que te de una patada giratoria. O sino a Jack Bauer, que últimamente Chuck anda muy ocupado resolviendo conflictos con los chinos.'), 403);
+     do_error(_('Esta página es sólo para administradores, sal de aquí, cojones ya.'), 403);
 }
-echo "</div>";
-echo "</div>"; // singlewrap
 do_footer();
