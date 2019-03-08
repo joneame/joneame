3ce760bfb5e34e3f77adfdf12b7fb975a627e7c7
wodim <neikokz@gmail.com>
2019-03-08 10:31:42 +0100

    Añado función que falta a rgdb

--- libs/rgdb.php
+++ libs/rgdb.php
@@ -388,6 +388,25 @@ class RGDB extends mysqli
 
         return $enum;
     }
+
+    public function garbitu($var, $quotes = true) {
+        if (is_array($var)) {   //run each array item through this function (by reference)
+            foreach ($var as &$val) {
+                $val = $this->escape($val);
+            }
+        } else if (is_string($var)) { //clean strings
+            $var = $this->escape($var);
+            if ($quotes) {
+                $var = "'${var}'";
+            }
+        }
+        else if (is_null($var)) {   //convert null variables to SQL NULL
+            $var = 'NULL';
+        } else if (is_bool($var)) {   //convert boolean variables to binary boolean
+            $var = ($var) ? 1 : 0;
+        }
+        return $var;
+    }
 }
 
 // Iterators inspired from:
