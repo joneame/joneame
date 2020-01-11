3b69cb5760fdfaa79b95040345372783e33a081c
wodim <neikokz@gmail.com>
2020-01-11 16:34:24 +0100

    paso la lista de emoticonos a una plantilla

--- ayuda.php
+++ ayuda.php
@@ -21,24 +21,19 @@ echo '<div class="ayuda-contenido">';
 if ($_REQUEST['id'] == 'faq') {
     Haanga::Load('help_faq.html');
 } elseif ($_REQUEST['id'] == 'emoticonos') {
-    echo '<h2>Lista de emoticonos</h2>';
-    // !!TODO(neiko 11/01/2020): esto hay que generarlo mediante código
-    echo '<table>';
-    echo '<tr><th>Emoticono</th><th class="vertical">Resultado</th><th>Emoticono</th><th class="vertical">Resultado</th><th>Emoticono</th><th>Resultado</th></tr>';
-    echo '<tr><td>:) :-)</td><td class="vertical">'.put_smileys('{smiley}').'</td><td>;) ;-)</td><td class="vertical">'.put_smileys('{wink}').'</td><td>:> :-></td><td>'.put_smileys('{wink}').'</td></tr>';
-    echo '<tr><td>:D :-D :grin:</td><td class="vertical">'.put_smileys('{grin}').'</td><td>&lt;:( &lt;:-( :oops:</td><td class="vertical">'.put_smileys('{oops}').'</td><td>:O :-O</td><td>'.put_smileys('{shocked}').'</td></tr>';
-    echo '<tr><td>&gt;&#58;(</td><td class="vertical">'.put_smileys('{angry}').'</td><td>?(</td><td class="vertical">'.put_smileys('{huh}').'</td><td>:-S :S</td><td>'.put_smileys('{confused}').'</td></tr>';
-    echo '<tr><td>8) 8-) 8D 8-D</td><td class="vertical">'.put_smileys('{cool}').'</td><td>:roll:</td><td class="vertical">'.put_smileys('{roll}').'</td><td>:\'( :\'-( :cry:</td><td>'.put_smileys('{cry}').'</td></tr>';
-    echo '<tr><td>:x :-x</td><td class="vertical">'.put_smileys('{lipssealed}').'</td><td>:/ :-/</td><td class="vertical">'.put_smileys('{undecided}').'</td><td>:* :-*</td><td>'.put_smileys('{kiss}').'</td></tr>';
-    echo '<tr><td>xD :lol:</td><td class="vertical">'.put_smileys('{lol}').'</td><td>:| :-|</td><td class="vertical">'.put_smileys('{blank}').'</td><td>:ffu:</td><td>'.put_smileys('{ffu}').'</td></tr>';
-    echo '<tr><td>:8: (8)</td><td class="vertical">'.put_smileys('{music}').'</td><td>:roto2:</td><td class="vertical">'.put_smileys('{roto}').'</td><td>:gaydude:</td><td>'.put_smileys('{gaydude}').'</td></tr>';
-    echo '<tr><td>:palm:</td><td class="vertical">'.put_smileys('{palm}').'</td><td>:goat: :goatse:</td><td class="vertical">'.put_smileys('{goatse}').'</td><td>o_o :wow:</td><td>'.put_smileys('{wow}').'</td></tr>';
-    echo '<tr><td>¬¬ :shame:</td><td class="vertical">'.put_smileys('{shame}').'</td><td>:sisi1:</td><td class="vertical">'.put_smileys('{sisi}').'</td><td>:nusenuse:</td><td>'.put_smileys('{nuse}').'</td></tr>';
-    echo '<tr><td>:P :-P</td><td class="vertical">'.put_smileys('{tongue}').'</td><td>:awesome:</td><td class="vertical">'.put_smileys('{awesome}').'</td><td>:alone:</td><td>'.put_smileys('{alone}').'</td></tr>';
-    echo '<tr><td>:trollface:</td><td class="vertical">'.put_smileys('{trollface}').'</td><td>:troll:</td><td class="vertical">'.put_smileys('{troll}').'</td><td>:yeah: :fuckyeah:</td><td>'.put_smileys('{yeah}').'</td></tr>';
-    echo '<tr><td>:clint:</td><td class="vertical">'.put_smileys('{clint}').'</td><td>:yaoface:</td><td class="vertical">'.put_smileys('{yaoface}').'</td><td>:longcat:</td><td>'.put_smileys('{longcat}').'</td></tr>';
-    echo '<tr><td>:cejas:</td><td class="vertical">'.put_smileys('{cejas}').'</td><td>:sisi3:</td><td class="vertical">'.put_smileys('{sisitres}').'</td></tr>';
-    echo '</table>';
+    $smileys_text = [':) :-)', ';) ;-)', ':> :->', ':D :-D :grin:', '<:( <:-( :oops:', ':O :-O',
+                     '>:(', '?( ?:( ?:-(', ':S :-S', '8) 8-) 8D 8-D :cool:', ':roll:', ":'( :'-( :cry:",
+                     ':x :-x', ':/ :-/', ':* :-*', 'xD x-D :lol:', ':| :-|', ':ffu:',
+                     ':8: (8)', ':roto2:', ':gaydude:', ':palm:', ':goat: :goatse:', 'o_o :wow:',
+                     '¬¬ :shame:', ':sisi1:', ':nusenuse:', ':P :-P', ':awesome:', ':alone:',
+                     ':trollface:', ':troll:', ':yeah: :fuckyeah:', ':clint:', ':yaoface:',
+                     ':longcat:', ':cejas:', ':sisi3:',];
+    $smileys = [];
+    foreach ($smileys_text as $smiley_text) {
+        $smileys += [$smiley_text => put_smileys(normalize_smileys(explode(' ', $smiley_text)[0]))];
+    }
+    $vars = compact('smileys');
+    Haanga::Load('help_smileys.html', $vars);
 } elseif ($_REQUEST['id'] == 'legal') {
     Haanga::Load('help_legal.html');
 } elseif ($_REQUEST['id'] == 'uso') {
