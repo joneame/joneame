6dbe15d6eee7106ccd0d280338022f141239c792
wodim <neikokz@gmail.com>
2020-01-30 00:24:51 +0100

    muestra año de voto en los cortos

--- backend/mostrar_votos_cortos.php
+++ backend/mostrar_votos_cortos.php
@@ -29,7 +29,7 @@ $corto = $db->get_row("select votos from cortos where id = $id ");
 echo '<div style="width:550px;padding: 5px 5px;text-align:left">';
 echo '<div style="padding-top: 20px;min-width:350px">';
 
-$votes = $db->get_results("SELECT vote_user_id, vote_value, user_avatar, user_login, date_format(vote_date,'%d/%m-%T') as date, UNIX_TIMESTAMP(vote_date) as ts,inet_ntoa(vote_ip_int) as ip FROM votes, users WHERE vote_type='cortos' and vote_link_id=$id AND vote_user_id > 0 AND user_id = vote_user_id ORDER BY vote_date DESC LIMIT $votes_offset,$votes_page_size");
+$votes = $db->get_results("SELECT vote_user_id, vote_value, user_avatar, user_login, date_format(vote_date,'%d/%m/%Y-%T') as date, UNIX_TIMESTAMP(vote_date) as ts,inet_ntoa(vote_ip_int) as ip FROM votes, users WHERE vote_type='cortos' and vote_link_id=$id AND vote_user_id > 0 AND user_id = vote_user_id ORDER BY vote_date DESC LIMIT $votes_offset,$votes_page_size");
 if ($votes) {
     echo '<div class="voters-list">';
     foreach ( $votes as $vote ) {
